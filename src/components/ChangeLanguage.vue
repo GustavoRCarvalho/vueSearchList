97f97a24b6ff49523774d225f93009d4f54a389e
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2024-01-19 17:27:23 -0300

    add: style to select language and accessibility title

--- src/components/ChangeLanguage.vue
+++ src/components/ChangeLanguage.vue
@@ -1,22 +1,31 @@
 <template>
-  <select v-on:change="setLocale">
-    {{
-      locale
-    }}
+  <select
+    title="Select Language"
+    class="selectLanguage"
+    v-on:change="setLocale"
+    v-bind:value="t.locale.value"
+  >
     <option value="en">English</option>
     <option value="pt">Portuguese</option>
   </select>
 </template>
 
 <script setup>
-import { ref } from "vue"
 import { useI18n } from "vue-i18n"
 
 const t = useI18n()
-
-const locale = ref(t.locale.value)
 
 function setLocale(e) {
   t.locale.value = e.target.value
 }
 </script>
+
+<style>
+.selectLanguage {
+  border: none;
+  outline: none;
+  font-family: Inter, Helvetica, Arial, sans-serif;
+  color: var(--color-white-medium);
+  background-color: var(--background-gray-medium);
+}
+</style>
